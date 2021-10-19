c50dd3021cdb9f91ebf78a53bfb166d270f5bee5
omerogur <oogur348@gmail.com>
2021-10-19 11:32:57 +0300

    18 ekim ders sonu

---
+++ javaScriptStarterKit-master/src/app.js
@@ -0,0 +1,12 @@
+console.log("Merhaba Kodlama.io")
+let dolarBgun = 9.30
+let dolarDun =9.20
+
+{
+    let dolarDun=9.10
+}
+console.log(dolarDun)
+
+let konutKredileri=["Konut Kredisi","Emlak Konut Kredisi","Kamu konut kredisi"]
+
+console.log(konutKredileri)
